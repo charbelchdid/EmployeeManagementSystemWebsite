0c69e0e0d0946dfe73700dc710a61e5828cbdae8
Charbel <charbel_chedid06@outlook.com>
2024-03-25 02:21:16 +0200

    Initial commit

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
     user: process.env.PGUSER, // Render sets PGUSER environment variable automatically
-    host: process.env.PGHOST || 'localhost',
+    host: process.env.PGHOST,
     database: 'project430',
     password: process.env.PGPASSWORD,
-    port: process.env.PGPORT || 5432,
+    port:  5432,
 });
 
 module.exports = pool;
