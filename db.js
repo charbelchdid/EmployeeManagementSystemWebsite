f90b47204c5fbc44c9367f3e5a35560294d79eeb
Charbel <charbel_chedid06@outlook.com>
2024-03-25 11:48:37 +0200

    Move files to root directory

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
-    user: process.env.PGUSER, // Render sets PGUSER environment variable automatically
-    host: process.env.PGHOST,
-    database: 'project430',
-    password: process.env.PGPASSWORD,
-    port:  5432,
-});
 
+const pool = new Pool({
+    user: 'project430_user',
+    host: 'dpg-co0at2fsc6pc738qqtng-a.oregon-postgres.render.com',
+    database: 'project430',
+    password: 'cU4b1JtlsSIxSCPn4XlwLWHNOFRvsS6V',
+    port: 5432,
+});
 module.exports = pool;
