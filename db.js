5c73fd7be05dd37f32a0ee77ee08282143f9337c
Charbel <charbel_chedid06@outlook.com>
2024-03-25 12:24:00 +0200

    Move files to root directory

--- db.js
+++ db.js
@@ -1,15 +1,16 @@
 const { Client } = require('pg');
 
-// Create a new PostgreSQL client instance
 const client = new Client({
     user: 'project430_user',
     host: 'dpg-co0at2fsc6pc738qqtng-a.oregon-postgres.render.com',
     database: 'project430',
     password: 'cU4b1JtlsSIxSCPn4XlwLWHNOFRvsS6V',
     port: 5432,
+    ssl: {
+        rejectUnauthorized: false // For testing purposes only; use a valid CA certificate in production
+    }
 });
 
-// Connect to the PostgreSQL database
 client.connect()
     .then(() => {
         console.log('Connected to PostgreSQL database');
