9ff478b5b4f0d02419029a880afc8fe2ae4cf72f
Charbel <charbel_chedid06@outlook.com>
2024-04-22 02:31:57 +0300

    frfr

--- managerEvents.js
+++ managerEvents.js
@@ -10,8 +10,8 @@ function formatDate(dateStr) {
 }
 function formatDateString(dateString) {
   const date = new Date(dateString);
-  const options = { day: '2-digit', year: '2-digit', month: 'long' };
-  return date.toLocaleDateString('en-US', options);
+  const options = { day: '2-digit', month: '2-digit', year: 'numeric' };
+  return new Intl.DateTimeFormat('en-GB', options).format(date);
 }
 
 // Route to get all events
